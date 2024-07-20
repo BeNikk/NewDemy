cf765232cfba5149d9fc3113c977917d9e3bf57e
BeNikk <bhattnik442@gmail.com>
2024-07-20 11:09:43 +0530

    video hls player added

--- newdemy/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ newdemy/app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,39 @@
-const ChapterIdPage = () => {
+import { getChapter } from "@/actions/getChapter";
+import { Banner } from "@/components/Banner";
+import { VideoPlayer } from "@/components/VideoPlayer";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage =async ({params}:{params:{courseId:string,chapterId:string}}) => {
+    const {userId}=auth();
+    if(!userId){
+        return redirect("/");
+    }
+    
+    const {chapter,course,muxData,attachments,userProgress,purchase,nextChapter}=await getChapter({userId,chapterId:params.chapterId,courseId:params.courseId});
+    if(!chapter || !course){
+        return redirect("/");
+    }
+
+    const isLocked=!chapter.isFree && !purchase;
+    const completeOnEnd=!!purchase && !userProgress?.isCompleted;
+
     return ( 
         <div>
-            Chapter id page;
+            {userProgress?.isCompleted && (
+                <Banner label="You already completed this chapter" variant={"success"}/>
+            )}
+            {isLocked && (
+                <Banner label="you need to purchase this course to watch this chapter" variant={"warning"}/>
+            )}
+            <div className="flex flex-col max-w-4xl mx-auto pb-20 ">
+                <div className="p-4">
+                    <VideoPlayer chapterId={params.chapterId} title={chapter.title} courseId={params.courseId} nextChapterId={nextChapter?.id!} playbackId={muxData?.playBackId!} isLocked={isLocked} completeOnEnd={completeOnEnd}/>
+
+                </div>
+
+
+            </div>
         </div>
      );
 }
---
+++ newdemy/components/VideoPlayer.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import {Loader2,Lock } from 'lucide-react';
+import { cn } from "@/lib/utils";
+import { useConfettiStore } from "@/hooks/use-confetti";
+
+interface VideoPlayerProps{
+    playbackId:string,
+    courseId:string,
+    chapterId:string,
+    nextChapterId?:string,
+    isLocked:boolean,
+    completeOnEnd:boolean,
+    title:string
+
+}
+
+export const VideoPlayer=({playbackId,courseId,chapterId,nextChapterId,isLocked,completeOnEnd,title}:VideoPlayerProps)=>{
+    const [isReady,setReady]=useState(false);
+    return(
+        <div className="relative aspect-video">
+            {!isReady && !isLocked && (
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+                    <Loader2 className="size-8 animate-spin text-secondary"/>
+
+                </div>
+            )}
+            {isLocked &&(
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+                    <Lock className="size-8 "/>
+                    <p className="text-sm">This chapter is locked</p>
+                </div>
+            )}
+            {!isLocked &&(
+                <MuxPlayer playbackId={playbackId} className={cn(!isReady && "hidden")} title={title} onCanPlay={()=>setReady(true)} onEnded={()=>{}} autoPlay/>
+            )}
+
+        </div>
+    )
+}
