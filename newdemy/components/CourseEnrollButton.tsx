8bc431ce6682979920ee18c47d1359fe346b7d88
BeNikk <bhattnik442@gmail.com>
2024-07-21 07:27:35 +0530

    feat:stripe payment integrated

--- newdemy/components/CourseEnrollButton.tsx
+++ newdemy/components/CourseEnrollButton.tsx
@@ -2,14 +2,33 @@
 
 import { formatPrice } from "@/lib/format";
 import { Button } from "./ui/button";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 interface CourseEnrollButtonProps{
     price:number,
     courseId:string
 }
 export const CourseEnrollButton=({price,courseId}:CourseEnrollButtonProps)=>{
+
+    const [isLoading,setisLoading]=useState(false);
+    async function onClick(){
+        try {
+            setisLoading(true);
+            const response=await axios.post(`/api/courses/${courseId}/checkout`);
+            window.location.assign(response.data.url);
+
+        } catch (error) {
+            toast.error("Something went wrong");
+            
+        }finally{
+            setisLoading(false);
+        }
+    }
+
     return(
-        <Button className="w-full md:w-auto" size={"sm"}>
+        <Button onClick={onClick} disabled={isLoading} className="w-full md:w-auto" size={"sm"}>
             Enroll for {formatPrice(price)}
         </Button>
     )
